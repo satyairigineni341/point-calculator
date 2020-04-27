47e7da98eeea7599c1972921447f883d8c53db21
satyairigineni341 <iriginenisatya@gmail.com>
2020-04-27 14:09:04 -0500

    swagger Implementation

--- point-calculator/src/main/java/com/pointcalculator/PointCalculatorApplication.java
+++ point-calculator/src/main/java/com/pointcalculator/PointCalculatorApplication.java
@@ -1,4 +1,4 @@
-package com.example.pointcalculator;
+package com.pointcalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
